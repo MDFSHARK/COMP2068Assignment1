3833a4e6cd3971a69f490e1b72b8f1fce7f561b2
Mason Ferrie <200410846@student.georgianc.on.ca>
2020-06-29 19:45:27 -0400

    Added pugs for all pages and routed them through index.js

--- COMP2106Assignment1/routes/index.js
+++ COMP2106Assignment1/routes/index.js
@@ -7,4 +7,24 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+/*Route to about me page */
+router.get('/aboutme', function (req, res) {
+    res.render('aboutme', { title: 'About Me' });
+});
+
+/*Route to projects page */
+router.get('/projects', function (req, res) {
+    res.render('projects', { title: 'Projects' });
+});
+
+/*Route to services page */
+router.get('/services', function (req, res) {
+    res.render('services', { title: 'Services' });
+});
+
+/*Route to contact me page */
+router.get('/contactme', function (req, res) {
+    res.render('contactme', { title: 'Contact Me' });
+});
+
 module.exports = router;
