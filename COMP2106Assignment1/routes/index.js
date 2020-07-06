880b3a2908f87b55f91b15682789c7ec9cf7edfd
Mason Ferrie <200410846@student.georgianc.on.ca>
2020-07-05 23:27:29 -0400

    Added hyperlinks to all routes in the header

--- COMP2106Assignment1/routes/index.js
+++ COMP2106Assignment1/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Welcome to Mason Ferries Personal Portfolio' });
 });
 
 /*Route to about me page */
